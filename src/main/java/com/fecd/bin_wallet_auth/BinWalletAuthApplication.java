012d88517f51314cae2cbef50e553ebcf5bb63b7
Fernando Emanuel <78227397+femm34@users.noreply.github.com>
2024-12-28 19:51:24 -0600

    first commit

---
+++ src/main/java/com/fecd/bin_wallet_auth/BinWalletAuthApplication.java
@@ -0,0 +1,13 @@
+package com.fecd.bin_wallet_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinWalletAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinWalletAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fecd/bin_wallet_auth/BinWalletAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fecd.bin_wallet_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinWalletAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
